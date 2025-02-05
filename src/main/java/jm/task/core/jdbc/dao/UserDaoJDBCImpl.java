8286aaf7d7f238070c4dc515bd4b1da3ae7b1dd2
rgobov <rgobov@gmail.com>
2025-02-05 16:29:25 +0200

    first commit

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,105 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
+    private final Connection conn;
+
+    {
+        try {
+            conn = Util.getConnection();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public UserDaoJDBCImpl() {
 
     }
 
     public void createUsersTable() {
+        try (Statement statement = conn.createStatement()) {
+            statement.executeUpdate(Util.CREATE_USERS_TABLE_MySQL);
+            System.out.println("Таблица создана");
 
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void dropUsersTable() {
+        try (Statement statement = conn.createStatement();) {
+            statement.executeUpdate(Util.DROP_USERS_TABLE_SQL);
+            System.out.println("Таблица удалена");
 
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
+        try (PreparedStatement preparedStatement = conn.prepareStatement(Util.SAVE_USER_SQL)) {
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, lastName);
+            preparedStatement.setByte(3, age);
+            preparedStatement.executeUpdate();
+            System.out.println("User с именем – " + name + " добавлен в базу данных");
 
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void removeUserById(long id) {
+        try (PreparedStatement statement = conn.prepareStatement(Util.REMOVE_USER_BY_ID_SQL)) {
+            statement.setLong(1, id);
+            statement.executeUpdate();
+            System.out.println("User удален");
 
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public List<User> getAllUsers() {
-        return null;
+        List<User> users = new ArrayList<User>();
+        try (Statement statement = conn.createStatement();
+             ResultSet resultSet = statement.executeQuery(Util.GET_ALL_USERS_SQL);) {
+            while (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getLong("id"));
+                user.setName(resultSet.getString("name"));
+                user.setLastName(resultSet.getString("lastName"));
+                user.setAge(resultSet.getByte("age"));
+                users.add(user);
+
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return users;
     }
 
     public void cleanUsersTable() {
+        try (Statement statement = conn.createStatement()) {
+            statement.executeUpdate(Util.CLEAN_USERS_TABLE_SQL);
+            System.out.println("Таблица очищена");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
+    private void closeConnection() {
+        try {
+            conn.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
